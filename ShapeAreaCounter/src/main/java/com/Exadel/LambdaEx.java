0dae7177756cecf46e221d85bd0df1438b75fa86
AurielKiss <aurelijus.kutra@gmail.com>
2015-11-05 14:39:11 +0200

    Commit test

---
+++ ShapeAreaCounter/src/main/java/com/Exadel/LambdaEx.java
@@ -0,0 +1,7 @@
+package com.Exadel;
+
+/**
+ * Created by User on 2015.09.30.
+ */
+public class LambdaEx {
+}
---
+++ ShapeAreaCounter/src/main/java/com/Exadel/c.java
@@ -0,0 +1,7 @@
+package com.Exadel;
+
+/**
+ * Created by User on 2015.09.21.
+ */
+public class c {
+}
