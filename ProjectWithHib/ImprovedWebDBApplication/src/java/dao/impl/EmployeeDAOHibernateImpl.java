775d6dbb02f9f8d71c76af6b899aa98c9af4af27
AurielKiss <aurelijus.kutra@gmail.com>
2015-11-12 17:33:34 +0200

    Working

--- ProjectWithHib/ImprovedWebDBApplication/src/java/dao/impl/EmployeeDAOHibernateImpl.java
+++ ProjectWithHib/ImprovedWebDBApplication/src/java/dao/impl/EmployeeDAOHibernateImpl.java
@@ -13,40 +13,79 @@ import org.apache.logging.log4j.Logger;
 import org.hibernate.Hibernate;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 @ManagedBean
 @ApplicationScoped
 public class EmployeeDAOHibernateImpl implements EmployeeDAO, Serializable {
+
+    List list = null;
 
     private static final Logger LOG = LogManager
             .getLogger(EmployeeDAOHibernateImpl.class);
 
     @Override
     public void saveEmployee(Employee employee) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        Session session = null;
+
+        session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        try {
+            if (employee.getId() == 0) {
+                employee.setId(list.size() + 1);
+                employee.setStreet("nepasirinkta");
+                session.save(employee);
+                tx.commit();
+                LOG.info("Padarytas naujas irasas");
+            } else {
+
+                session.update(employee);
+                tx.commit();
+            }
+
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+
+            session.close();
+        }
+
     }
 
     @Override
-    public Employee load(int id) {
+    public Employee load(int id
+    ) {
         Query query = null;
         Session session = null;
         Employee employee = null;
         try {
-            
+
             session = HibernateUtil.getSessionFactory().openSession();
-            //session.beginTransaction();
-            query = session.createQuery("from beans.EmployeeBean where id=" + id);
-            query.setMaxResults(1);
-            LOG.info("1  Mano employeris -> " + employee+" id: "+id);
+//            Session session2 = SessionUtil.getSession();
+//            Transaction tx = session.beginTransaction();
+//            query = session.createQuery("from beans.EmployeeBean where id=" + id);
+//            query.setMaxResults(1);
+//
+            LOG.info("Mano employeris turi but null-> " + employee + " id: " + id);
+//            employee = new Employee();
 //            employee = (Employee) query.uniqueResult();
 //            LOG.info("2  Mano employeris -> " + employee);
-            employee = (Employee) session.get(EmployeeBean.class, id); //session.get session.load
+            employee = (Employee) session.get(Employee.class, id); //session.get session.load
+//            Hibernate.initialize(employee);
+//            LOG.info("Before commit-> " + employee);
+//            tx.commit();
             if (employee == null) {
                 LOG.info("employee not found for id " + id);
             }
         } catch (Exception e) {
+            e.printStackTrace();
         } finally {
-            LOG.info("Mano employeris -> " + employee);
+            LOG.info("Mano employeris not null -> " + employee);
             if (session != null && session.isOpen()) {
                 session.close();
             }
@@ -58,10 +97,10 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO, Serializable {
     @Override
     public List<Employee> getAllEmployeesFromDb() {
         Session session = null;
-        List list = null;
+
         try {
             session = HibernateUtil.getSessionFactory().openSession();
-            Query query = session.createQuery("from beans.EmployeeBean");
+            Query query = session.createQuery("from entity.Employee");
             list = query.list();
         } catch (Exception e) {
         } finally {
